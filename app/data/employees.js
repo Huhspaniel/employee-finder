481ce1983da49483704aca3492b237d85e976122
Huhspaniel <41580234+Huhspaniel@users.noreply.github.com>
2018-11-02 23:12:19 -0400

    Change starting employee data

--- app/data/employees.js
+++ app/data/employees.js
@@ -16,19 +16,19 @@ module.exports = [
         ]
     },
     {
-        "name": "Sam",
-        "photo": "",
+        "name": "Daniel Ferreira",
+        "photo": "https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/44828964_1679802805459763_1820246464765362176_o.jpg?_nc_cat=105&_nc_ht=scontent-dfw5-2.xx&oh=3d4fb5ef4c2b83f20390d77f98df1d1b&oe=5C804117",
         "scores": [
-            4,
             3,
+            4,
+            4,
             2,
+            4,
             1,
             5,
             3,
             1,
-            4,
-            5,
-            2
+            3
         ]
     }
 ]
